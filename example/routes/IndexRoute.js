e269631e0290a44a6918375f0a40afccc3dd9538
fabosch <faboschpublic@gmail.com>
2019-08-23 23:42:50 +0200

    renamed package to express.js-ratelimiter

--- example/routes/IndexRoute.js
+++ example/routes/IndexRoute.js
@@ -1,4 +1,4 @@
-// const { ExpressRoute } = require('express-ratelimiter'); // you need to use this code
+// const { ExpressRoute } = require('express.js-ratelimiter'); // you need to use this code
 const { ExpressRoute } = require('../../');
 
 class IndexRoute extends ExpressRoute
