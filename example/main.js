383b2e74fb7ebfda9b84ddfffad54622ffb24213
fabosch <faboschpublic@gmail.com>
2019-08-24 10:39:30 +0200

    added the default values to the readme

--- example/main.js
+++ example/main.js
@@ -5,8 +5,8 @@ const app = express();
 const IndexRoute = require('./routes/IndexRoute');
 const indexRoute = new IndexRoute(
     {
-        timePassedBetween: 1000, // 1000ms
-        maxPerMinute: 3 // max amount of requests per minute
+        timePassedBetween: 1000, // 1000ms, the min time passed between two requests. DEFAULT: 50ms
+        maxPerMinute: 3 // max amount of requests per minute. DEFAULT: Number.MAX_SAFE_INTEGER
     });
 
 app.get('/', (req, res, next) =>
